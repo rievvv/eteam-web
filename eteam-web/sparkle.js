2d61f04967437d4528b7249030a35d37ec76879e
rievvv <162405729+rievvv@users.noreply.github.com>
2025-05-17 03:20:04 +0700

    Delete eteam-web directory

--- eteam-web/sparkle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('mousemove', function (e) {
-  const spark = document.createElement('span');
-  spark.className = 'global-spark';
-  spark.style.left = e.pageX + 'px';
-  spark.style.top = e.pageY + 'px';
-  document.body.appendChild(spark);
-  setTimeout(() => spark.remove(), 600);
-});
